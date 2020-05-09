aa686b73863a8c58b47b73ea35cf187096797eb5
NemsisRE <nemesissre@gmail.com>
2020-05-09 01:45:14 +0000

    removed markercolor Default reference

--- addons/bft/CfgMarkerColors.hpp
+++ addons/bft/CfgMarkerColors.hpp
@@ -1,80 +1,74 @@
 class CfgMarkerColors {
-    class Default;
-
-    class qipBFT_bft_colors: Default {
-        name = "qipBFT Colors"
+    class Color_MAIN_Default {
         author = "NemesisRE";
         scope = 1;
-    };
-    // Default Colors
-    class Color_MAIN_Default: qipBFT_bft_colors {
         name = "qipBFT Main";
         color[] = {1, 1, 1, 1};
     };
-    class Color_RED_Default: qipBFT_bft_colors {
+    class Color_RED_Default: Color_MAIN_Default {
         name = "qipBFT Red";
         color[] = {0.901, 0, 0, 1};
     };
-    class Color_GREEN_Default: qipBFT_bft_colors {
+    class Color_GREEN_Default: Color_MAIN_Default {
         name = "qipBFT Green";
         color[] = {0, 0.901, 0, 1};
     };
-    class Color_BLUE_Default: qipBFT_bft_colors {
+    class Color_BLUE_Default: Color_MAIN_Default {
         name = "qipBFT Blue";
         color[] = {0, 0.360, 1, 1};
     };
-    class Color_YELLOW_Default: qipBFT_bft_colors {
-        name = "qipBFT Yellow"
+    class Color_YELLOW_Default: Color_MAIN_Default {
+        name = "qipBFT Yellow";
         color[] = {0.901, 0.901, 0, 1};
     };
     // ACE Colors
-    class Color_MAIN_ACE: qipBFT_bft_colors {
+    class Color_MAIN_ACE: Color_MAIN_Default {
         name = "qipBFT Main (ACE)";
         color[] = {1,1,1,1};
     };
-    class Color_RED_ACE: qipBFT_bft_colors {
+    class Color_RED_ACE: Color_MAIN_Default {
         name = "qipBFT Red (ACE)";
         color[] = {1, 0.670, 0.670, 1};
     };
-    class Color_GREEN_ACE: qipBFT_bft_colors {
+    class Color_GREEN_ACE: Color_MAIN_Default {
         name = "qipBFT Green (ACE)";
         color[] = {0.670, 1, 0.670, 1};
     };
-    class Color_BLUE_ACE: qipBFT_bft_colors {
+    class Color_BLUE_ACE: Color_MAIN_Default {
         name = "qipBFT Blue (ACE)";
         color[] = {0.670, 0.670, 1, 1};
     };
-    class Color_YELLOW_ACE: qipBFT_bft_colors {
+    class Color_YELLOW_ACE: Color_MAIN_Default {
         name = "qipBFT Yellow (ACE)";
         color[] = {1, 1, 0.670, 1};
     };
     // SThud Colors
-    class Color_MAIN_STHUD: qipBFT_bft_colors {
+    class Color_MAIN_STHUD: Color_MAIN_Default {
         name = "qipBFT Main (STHUD)";
         color[] = {1,1,1,1};
     };
-    class Color_RED_STHUD: qipBFT_bft_colors {
+    class Color_RED_STHUD: Color_MAIN_Default {
         name = "qipBFT Red (STHUD)";
         color[] = {0.901, 0, 0, 1};
     };
-    class Color_GREEN_STHUD: qipBFT_bft_colors {
+    class Color_GREEN_STHUD: Color_MAIN_Default {
         name = "qipBFT Green (STHUD)";
         color[] = {0, 0.8, 0, 1};
     };
-    class Color_BLUE_STHUD: qipBFT_bft_colors {
+    class Color_BLUE_STHUD: Color_MAIN_Default {
         name = "qipBFT Blue (STHUD)";
         color[] = {0.2, 0.2, 1, 1};
     };
-    class Color_YELLOW_STHUD: qipBFT_bft_colors {
+    class Color_YELLOW_STHUD: Color_MAIN_Default {
         name = "qipBFT Yellow (STHUD)";
         color[] = {0.850, 0.850, 0, 1};
     };
     // Additional Colors
-    class Color_INCAPACITATED: qipBFT_bft_colors {
+    class Color_INCAPACITATED: Color_MAIN_Default {
         name = "qipBFT Incapacitated";
         color[] = {0.701, 0.701, 0.701, 1};
     };
-    class Color_INJURED: qipBFT_bft_colors {
+    class Color_INJURED: Color_MAIN_Default {
         name = "qipBFT Injured";
         color[] = {0.858, 0.584, 0.439, 1};
     };
